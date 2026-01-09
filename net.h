43e609655ffc1d1c2c5d805007bb21fe42de5b89
Grigorev Alexey <litk1pl@gmail.com>
2026-01-09 19:55:19 +0300

    update net.h

--- net.h
+++ net.h
@@ -4,7 +4,23 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+#include <unistd.h>
 
-void swap_endian(void *ptr, size_t size);
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define MAXLINE 4096
+#define LISTENQ 1024
+
+#define DAYTIME_PORT 13
+
+void err_quit(const char* format, ...);
+void err_sys(const char* format, ...);
+void err_msg(const char* format, ...);
 
 #endif
