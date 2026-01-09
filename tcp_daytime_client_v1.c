c1fd36a9241f0193a3eb85c3e3289752b9465875
Grigorev Alexey <litk1pl@gmail.com>
2026-01-09 19:54:27 +0300

    tcp daytime client version 1

---
+++ tcp_daytime_client_v1.c
@@ -0,0 +1,35 @@
+#include "net.h"
+
+int main(int argc, char** argv)
+{
+  int sockfd, s, n;
+  char buf[MAXLINE + 1];
+  struct sockaddr_in serv_addr;
+
+  if (argc != 2)
+    err_quit("Usage: %s <IP address>\n", argv[0]);
+
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    err_sys("socket error\n");
+
+  bzero(&serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  if ((s = inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)) <= 0) {
+    if (s == 0)
+      err_quit("Invalid IP address\n");
+    else
+      err_sys("inet_pton error\n");
+  }
+  serv_addr.sin_port = htons(DAYTIME_PORT);
+
+  if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+    err_sys("connect error\n");
+
+  while ((n = read(sockfd, buf, MAXLINE)) > 0) {
+    buf[n] = '\0';
+    if (fputs(buf, stdout) == EOF)
+      err_quit("fputs error");
+  }
+  if (n < 0)
+    err_sys("read error");
+}
